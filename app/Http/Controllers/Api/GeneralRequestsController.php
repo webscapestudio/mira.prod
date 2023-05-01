aef94cfb2dac11eaafd22f77ae1d5059c3beebe7
Sunat Boev <sunat.boev@tcell.tj>
2023-05-01 17:05:26 +0500

    UPD: Http request without ssl verified

--- app/Http/Controllers/Api/GeneralRequestsController.php
+++ app/Http/Controllers/Api/GeneralRequestsController.php
@@ -28,43 +28,46 @@ class GeneralRequestsController extends Controller
         $input = $request->json()->all();
 
         $validator = Validator::make($input, [
-            'name'       => 'required',
-            'phone'      => 'required',
-            'email'      => 'required|email',
-            'name_form'  => 'required',
-            'id_form'    => 'required',
-            'comment'    => 'required',
-            'domain'     => 'required',
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'name_form' => 'required',
+            'id_form' => 'required',
+            'comment' => 'required',
+            'domain' => 'required',
             'utm_source' => 'nullable',
             'utm_medium' => 'nullable',
-            'utm_term'   => 'nullable',
-            'utm_content'=> 'nullable',
+            'utm_term' => 'nullable',
+            'utm_content' => 'nullable',
             'utm_campaign' => 'nullable',
         ]);
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()]);
         }
         $gen_request = [
-            'name' =>  $input['name'],
+            'name' => $input['name'],
             'phone' => $input['phone'],
         ];
 
         $amoCRM_request = $gen_request + [
-            'email' => $input['email'],
-            'name_form' => $input['name_form'],
-            'id_form' => $input['id_form'],
-            'comment' => $input['comment'],
-            'geoip' => geoip($request->ip()),
-            'domain' => $input['domain'],
-            'utm_source' => $input['utm_source'] ?? env('UTM_SOURCE'),
-            'utm_medium' => $input['utm_medium'] ?? env('UTM_MEDIUM'),
-            'utm_term' => $input['utm_term'] ?? env('UTM_TERM'),
-            'utm_content' => $input['utm_content'] ?? env('UTM_CONTENT'),
-            'utm_campaign' => $input['utm_campaign'] ?? env('UTM_CAMPAIGN'),
-        ];
+                'email' => $input['email'],
+                'name_form' => $input['name_form'],
+                'id_form' => $input['id_form'],
+                'comment' => $input['comment'],
+                'geoip' => geoip($request->ip()),
+                'domain' => $input['domain'],
+                'utm_source' => $input['utm_source'] ?? env('UTM_SOURCE'),
+                'utm_medium' => $input['utm_medium'] ?? env('UTM_MEDIUM'),
+                'utm_term' => $input['utm_term'] ?? env('UTM_TERM'),
+                'utm_content' => $input['utm_content'] ?? env('UTM_CONTENT'),
+                'utm_campaign' => $input['utm_campaign'] ?? env('UTM_CAMPAIGN'),
+            ];
 
         try {
-            $response = Http::post(env('AMO_CRM_URL'), $amoCRM_request);
+            $response = Http::withOptions([
+                'verify' => false
+            ])
+                ->post(env('AMO_CRM_URL'), $amoCRM_request);
 
             if ($response->failed()) {
                 return response()->json([
@@ -73,7 +76,7 @@ class GeneralRequestsController extends Controller
                 ]);
             }
 
-            Mail::send('mails.general_request', $gen_request, function($message)use($gen_request) {
+            Mail::send('mails.general_request', $gen_request, function ($message) use ($gen_request) {
                 $message->to(env('MAIL_TO_ADDRESS'))                                            //почта
                 ->subject('General Request Email');
             });
